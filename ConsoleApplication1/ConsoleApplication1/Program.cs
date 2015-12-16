60f5867952c8b0d4166708b4056a819b8ec6f655
tamir.dresher@gmail.com <vhpupuyo>
2015-12-16 10:01:25 +0200

    asdssdds

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello Git from branch 123");
         }
     }
 }
